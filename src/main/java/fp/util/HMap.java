da3a458ac9c0bbb2c9b97e529c504437f4607c88
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2023-02-25 21:15:34 +0100

    Add HMap

---
+++ src/main/java/fp/util/HMap.java
@@ -0,0 +1,66 @@
+package fp.util;
+
+import java.util.stream.Collectors;
+import java.util.Optional;
+
+public class HMap {
+    private final HList hlist;
+
+    private <K, V> HMap(HList hlist) {
+        this.hlist = hlist;
+    }
+
+    public static <K, V> HMap of(final K key, final V value) {
+        final HList hlist = HList.of(Tuple2.of(key, value));
+        return new HMap(hlist);
+    }
+
+    public <K, V> HMap add(final K key, final V value) {
+        final HList newHlist = hlist.add(Tuple2.of(key, value));
+        return new HMap(newHlist);
+    }
+
+    public int size() {
+        return hlist.size();
+    }
+
+    public <K, V> Optional<V> getValue(K key) {
+        return hlist.valueStream()
+            .map(Tuple2.class::cast)
+            .filter(tuple -> tuple._1().equals(key))
+            .map(tuple -> (V) tuple._2())
+            .findAny();
+    }
+
+    public <K> HList getValues(K key) {
+        return hlist.valueStream()
+            .map(Tuple2.class::cast)
+            .filter(tuple -> tuple._1().equals(key))
+            .map(tuple -> tuple._2())
+            .reduce((HList) new HNil(), HList::add, (h1, h2) -> h1);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof HMap)) {
+            return false;
+        }
+
+        final HMap other = (HMap) obj;
+
+        return other.hlist.equals(hlist);
+    }
+
+    @Override
+    public String toString() {
+        final String values = hlist.valueStream()
+            .map(Tuple2.class::cast)
+            .map(tuple -> tuple._1() + " -> " + tuple._2())
+            .collect(Collectors.joining(", "));
+        return "HMap(" + values + ")";
+    }
+}
---
+++ src/test/java/fp/util/HMapTest.java
@@ -0,0 +1,88 @@
+package fp.util;
+
+import java.util.function.Function;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HMapTest {
+
+    @Test
+    public void testToString() {
+        final HMap hmap = HMap.of("int", 13)
+            .add("str", "String")
+            .add("double", 3.45)
+            .add("optional", Optional.empty())
+            .add("long", 15L);
+
+        Assert.assertEquals(
+            hmap.toString(),
+            "HMap(long -> 15, optional -> Optional.empty, double -> 3.45, str -> String, int -> 13)"
+        );
+    }
+
+    @Test
+    public void testSize() {
+        final HMap hmap = HMap.of("int", 13)
+            .add("str", "String")
+            .add("double", 3.45)
+            .add("optional", Optional.empty())
+            .add("long", 15L);
+
+        Assert.assertEquals(hmap.size(), 5);
+    }
+
+    @Test
+    public void testGetValue() {
+        final HMap hmap = HMap.of("int", 13)
+            .add("str", "String")
+            .add("double", 3.45)
+            .add("optional", Optional.empty())
+            .add("long", 15L);
+
+        final Optional<Double> value = hmap.getValue("double");
+
+        Assert.assertEquals(value, Optional.of(3.45));
+    }
+
+    @Test
+    public void testGetValueMissing() {
+        final HMap hmap = HMap.of("int", 13)
+            .add("str", "String")
+            .add("double", 3.45)
+            .add("optional", Optional.empty())
+            .add("long", 15L);
+
+        final Optional<Double> value = hmap.getValue("key");
+
+        Assert.assertEquals(value, Optional.empty());
+    }
+
+    @Test
+    public void testGetValues() {
+        final HMap hmap = HMap.of("number", 13)
+            .add("str", "String")
+            .add("number", 3.45)
+            .add("optional", Optional.empty())
+            .add("number", 15L);
+
+        final HList hlist = hmap.getValues("number");
+
+        Assert.assertEquals(hlist, HList.of(15L).add(3.45).add(13));
+    }
+
+    @Test
+    public void testGetValuesMissing() {
+        final HMap hmap = HMap.of("number", 13)
+            .add("str", "String")
+            .add("number", 3.45)
+            .add("optional", Optional.empty())
+            .add("number", 15L);
+
+        final HList hlist = hmap.getValues("key");
+
+        Assert.assertEquals(hlist, new HNil());
+    }
+}
