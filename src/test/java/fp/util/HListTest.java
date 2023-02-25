767288bc15544f673030c146139550713c8ba2c1
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2023-02-25 17:29:40 +0100

    Add bigTest

--- src/test/java/fp/util/HListTest.java
+++ src/test/java/fp/util/HListTest.java
@@ -1,6 +1,8 @@
 package fp.util;
 
+import java.util.function.Function;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,6 +34,15 @@ public class HListTest {
     public void testFloatItem() {
         HItem<Long, HItem<Optional<Object>, HItem<Float, HItem<String, HItem<Integer, HNil>>>>> hlist =
             HList.of(13).add("String").add(3.45f).add(Optional.empty()).add(15L);
-        Assert.assertEquals(hlist.tail().tail().get(), (Float) 3.45f);
+        Assert.assertEquals(hlist.tail().tail().head(), (Float) 3.45f);
+    }
+
+    @Test
+    public void testBigList() {
+        final HList hlist = IntStream.range(0, 100_000)
+            .mapToObj(i -> i)
+            .reduce(new HNil(), (HList hl, Integer i) -> hl.add(i), (h1, h2) -> h1);
+        final Integer head = hlist.reverse().head();
+        Assert.assertEquals(head, (Integer) 0);
     }
 }
