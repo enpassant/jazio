aa8c236a3c828e8037c0ccb77808c68dd3aa08ea
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2023-02-25 22:12:27 +0100

    Add HList.addAll test

--- src/test/java/fp/util/HListTest.java
+++ src/test/java/fp/util/HListTest.java
@@ -16,25 +16,43 @@ public class HListTest {
 
     @Test
     public void testEquals() {
-        HList hlist = HList.of(13).add("String").add(3.45).add(Optional.empty()).add(15L);
-        HItem<Long, HItem<Optional<Object>, HItem<Double, HItem<String, HItem<Integer, HNil>>>>> hlist2 =
+        final HList hlist = HList.of(13)
+            .add("String")
+            .add(3.45)
+            .add(Optional.empty())
+            .add(15L);
+        final HItem<Long, HItem<Optional<Object>, HItem<Double, HItem<String, HItem<Integer, HNil>>>>> hlist2 =
             HList.of(13).add("String").add(3.45).add(Optional.empty()).add(15L);
         Assert.assertEquals(hlist, hlist2);
     }
 
     @Test
     public void testNotEquals() {
-        HList hlist = HList.of(13).add("String").add(3.45).add(Optional.empty()).add(15L);
-        HItem<Long, HItem<Optional<Object>, HItem<Float, HItem<String, HItem<Integer, HNil>>>>> hlist2 =
+        final HList hlist = HList.of(13)
+            .add("String")
+            .add(3.45)
+            .add(Optional.empty())
+            .add(15L);
+        final HItem<Long, HItem<Optional<Object>, HItem<Float, HItem<String, HItem<Integer, HNil>>>>> hlist2 =
             HList.of(13).add("String").add(3.45f).add(Optional.empty()).add(15L);
         Assert.assertNotEquals(hlist, hlist2);
     }
 
     @Test
     public void testFloatItem() {
-        HItem<Long, HItem<Optional<Object>, HItem<Float, HItem<String, HItem<Integer, HNil>>>>> hlist =
+        final HItem<Long, HItem<Optional<Object>, HItem<Float, HItem<String, HItem<Integer, HNil>>>>> hlist =
             HList.of(13).add("String").add(3.45f).add(Optional.empty()).add(15L);
         Assert.assertEquals(hlist.tail().tail().head(), (Float) 3.45f);
+    }
+
+    @Test
+    public void testAddAll() {
+        final HList hlist1 = HList.of(13).add("String").add(3.45);
+        final HList hlist2 = HList.of(15L).add(Optional.empty());
+
+        final HList hlist =
+            HList.of(13).add("String").add(3.45).add(Optional.empty()).add(15L);
+        Assert.assertEquals(hlist, hlist1.addAll(hlist2));
     }
 
     @Test
